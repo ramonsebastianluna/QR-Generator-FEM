8487bec13060b86f8dec8b605916c2b2def5c4d7
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-03-21 14:19:47 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+    return (
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="#">Ramón Sebastián Luna</a>.
+        </div>
+    )
+}
+
+export default App
